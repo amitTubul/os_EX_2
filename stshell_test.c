2aa261091ed8bcc7c3ca8e12bc41fde9f296c39d
itamarq <itamarq@gmail.com>
2023-04-28 16:52:09 +0300

    edited stshell_test.c to work with file writing output to file

--- stshell_test.c
+++ stshell_test.c
@@ -2,16 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <sys/wait.h>
 
-int containsWriteTo
+//
+int containsWriteTo(char* command){
+    for (int i = 0; i < strlen(command); ++i) {
+        if(command[i] == '>') return 1;
+    }
+    return 0;
+}
+
+int getPosOfGreaterSign(char* command){
+    for (int i = 0; i < strlen(command); ++i) {
+        if(command[i] == '>') return i;
+    }
+    return 0;
+}
+
+int containsAppendTo(char* command){
+    for (int i = 0; i < strlen(command) - 1; ++i) {
+        if(command[i] == '>' && command[i+1] == '>') return 1;
+    }
+    return 0;
+}
 
 
-int main()
-{
+int main() {
     char *command;
+    char currentCommand[100];
+    char givenFIleName[100];
     char **commands;
-    int size, i;
-    while(1) {
+    FILE* file;
+    int size, i, pipeFlag =0, signPos,fd, pid;
+    int pipefd[2];
+    while (1) {
         printf("stshell: ");
         size = 10; // initial size of the command string
         command = (char *) malloc(size * sizeof(char));
@@ -36,57 +61,203 @@ int main()
 
         // Remove the newline character from the end of the command string
         command[strlen(command) - 1] = '\0';
-        if(strcmp("exit", command)==0){
+        if (strcmp("exit", command) == 0) {
             break;
         }
 
 
-    // Split the command string by "|"
-    int num_commands = 1;
-    for (i = 0; i < strlen(command); i++) {
-        if (command[i] == '|') {
-            num_commands++;
+        // Split the command string by "|"
+        int num_commands = 1;
+        for (i = 0; i < strlen(command); i++) {
+            if (command[i] == '|') {
+                num_commands++;
+            }
         }
-    }
-    commands = (char**)malloc(num_commands * sizeof(char*));
-    if (commands == NULL) {
-        printf("Memory allocation failed\n");
-        return 1;
-    }
-
-    char *token = strtok(command, "|");
-    i = 0;
-    while (token != NULL) {
-        commands[i] = (char*)malloc((strlen(token) + 1) * sizeof(char));
-        if (commands[i] == NULL) {
+        commands = (char **) malloc(num_commands * sizeof(char *));
+        if (commands == NULL) {
             printf("Memory allocation failed\n");
             return 1;
         }
-        strcpy(commands[i], token);
-        token = strtok(NULL, "|");
-        i++;
+
+        char *token = strtok(command, "|");
+        i = 0;
+        while (token != NULL) {
+            commands[i] = (char *) malloc((strlen(token) + 1) * sizeof(char));
+            if (commands[i] == NULL) {
+                printf("Memory allocation failed\n");
+                return 1;
+            }
+            strcpy(commands[i], token);
+            token = strtok(NULL, "|");
+            i++;
+        }
+        if(num_commands > 1){
+            pipeFlag = 1;
+        }
+        pipe(pipefd);
+        for (int j = 0; j < num_commands; ++j) {
+            if (containsAppendTo(commands[j])) {
+                token = strtok(command, ">>");
+
+                // copy the first token (command) into currentCommand
+                strncpy(currentCommand, token, 99);
+
+                // get the second token (file name)
+                token = strtok(NULL, ">>");
+
+                // remove leading and trailing whitespace from the second token
+                i = 0;
+                while (isspace(token[i])) {
+                    i++;
+                }
+                j = strlen(token) - 1;
+                while (isspace(token[j])) {
+                    j--;
+                }
+                token[j+1] = '\0';
+
+                strncpy(givenFIleName, &token[i], 99);
+
+                file = fopen(givenFIleName, "a");
+                if (file == NULL){
+                    perror("File open failed");
+                    exit(1);
+                }
+                fd = fileno(file);
+                pid = fork();
+                if(pid==-1){
+                    perror("fork error");
+                    exit(EXIT_FAILURE);
+                }
+                if (pid == 0){
+                    int argc = 0;
+                    char* argv[10];
+
+                    char* token = strtok(currentCommand, " ");
+                    while (token != NULL) {
+                        argv[argc++] = token;
+                        token = strtok(NULL, " ");
+                    }
+                    argv[argc] = NULL;
+                    dup2(fd, STDOUT_FILENO);
+                    execvp(argv[0], argv);
+                    fclose(file);
+                    exit(EXIT_SUCCESS);
+
+                }
+                wait(NULL);
+            }
+            else if(containsWriteTo(commands[j])){
+                signPos = getPosOfGreaterSign(commands[j]);
+                token = strtok(command, ">");
+
+                // copy the first token (command) into currentCommand
+                strncpy(currentCommand, token, 99);
+
+                // get the second token (file name)
+                token = strtok(NULL, ">");
+
+                // remove leading and trailing whitespace from the second token
+                i = 0;
+                while (isspace(token[i])) {
+                    i++;
+                }
+                j = strlen(token) - 1;
+                while (isspace(token[j])) {
+                    j--;
+                }
+                token[j+1] = '\0';
+
+                strncpy(givenFIleName, &token[i], 99);
+
+                file = fopen(givenFIleName, "w");
+                if (file == NULL){
+                    perror("File open failed");
+                    exit(1);
+                }
+                fd = fileno(file);
+                pid = fork();
+                if(pid==-1){
+                    perror("fork error");
+                    exit(EXIT_FAILURE);
+                }
+                if (pid == 0){
+                    int argc = 0;
+                    char* argv[10];
+
+                    char* token = strtok(currentCommand, " ");
+                    while (token != NULL) {
+                        argv[argc++] = token;
+                        token = strtok(NULL, " ");
+                    }
+                    argv[argc] = NULL;
+                    dup2(fd, STDOUT_FILENO);
+                    execvp(argv[0], argv);
+                    fclose(file);
+                    exit(EXIT_SUCCESS);
+
+                }
+                wait(NULL);
+            }else{
+                if (j == 0){
+                    pid = fork();
+                    if(pid==-1){
+                        perror("fork error");
+                        exit(EXIT_FAILURE);
+                    }
+                    if (pid == 0){
+                        int argc = 0;
+                        char* argv[10];
+
+                        char* token = strtok(currentCommand, " ");
+                        while (token != NULL) {
+                            argv[argc++] = token;
+                            token = strtok(NULL, " ");
+                        }
+                        argv[argc] = NULL;
+                        if(pipeFlag){
+                            close(pipefd[1]);
+                            dup2(pipefd[1], STDOUT_FILENO);
+                        }
+                        execvp(argv[0], argv);
+
+                        exit(EXIT_SUCCESS);
+
+                    }
+                    wait(NULL);
+                }
+                pid = fork();
+                if(pid==-1){
+                    perror("fork error");
+                    exit(EXIT_FAILURE);
+                }
+                if (pid == 0){
+                    int argc = 0;
+                    char* argv[10];
+
+                    char* token = strtok(commands[j], " ");
+                    while (token != NULL) {
+                        argv[argc++] = token;
+                        token = strtok(NULL, " ");
+                    }
+                    argv[argc] = NULL;
+                    //TODO: add output redirection to pipe if the flag is true.
+                    execvp(argv[0], argv);
+                    exit(EXIT_SUCCESS);
+                }
+                wait(NULL);
+            }
+
+        }
+
+        // Free the memory
+        for (i = 0; i < num_commands; i++) {
+            free(commands[i]);
+        }
+        free(commands);
+        free(command);
+
+        return 0;
+
     }
-
-    // Print the commands
-    printf("The commands are:\n");
-        pipe()
-    for (i = 0; i < num_commands; i++) {
-        fork()
-        dup2(pipefd[1], STDOUT_FILENO)
-        read(pipfd[0])
-        execute
-
-
-
-    }
-
-    // Free the memory
-    for (i = 0; i < num_commands; i++) {
-        free(commands[i]);
-    }
-    free(commands);
-    free(command);
-
-    return 0;
-
 }
--- test_pipe.c
+++ test_pipe.c
@@ -1,57 +1,61 @@
-////
-//// Created by itamarq on 4/28/23.
-////
-//#include <stdio.h>
-//#include <unistd.h>
-//#include <sys/types.h>
-//#include <sys/wait.h>
-//
-//int main()
-//{
-//    int pipefd[2];
-//    pid_t pid;
-//
-//    // create pipe
-//    if (pipe(pipefd) == -1) {
-//        perror("pipe");
-//        return 1;
-//    }
-//
-//    // create child process
-//    pid = fork();
-//    if (pid == -1) {
-//        perror("fork");
-//        return 1;
-//    }
-//
-//    if (pid == 0) { // child process
-//        // close read end of pipe
-//        close(pipefd[0]);
-//
-//        // redirect stdout to write end of pipe
-//        dup2(pipefd[1], STDOUT_FILENO);
-//
-//        // execute command1
-//        execlp("ls", "ls", NULL);
-//        perror("execlp");
-//        return 1;
-//    } else { // parent process
-//        // close write end of pipe
-//        close(pipefd[1]);
-//
-//        // redirect stdin to read end of pipe
-//        dup2(pipefd[0], STDIN_FILENO);
-//
-//        // execute command2
-//        execlp("grep", "grep", "example", NULL);
-//        perror("execlp");
-//        return 1;
-//    }
-//
-//    // close pipe ends and wait for child process to finish
-//    close(pipefd[0]);
-//    close(pipefd[1]);
-//    wait(NULL);
-//
-//    return 0;
-//}
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main()
+{
+    int fd[2]; // file descriptors for the read and write ends of the pipe
+    pid_t pid1, pid2;
+    char buffer[20];
+
+    // create the pipe
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    // fork the first child process
+    pid1 = fork();
+
+    if (pid1 == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid1 == 0) { // first child process
+        close(fd[1]); // close the write end of the pipe
+        read(fd[0], buffer, sizeof(buffer)); // read from the read end of the pipe
+        printf("First child process received: %s\n", buffer);
+
+        close(fd[0]); // close the read end of the pipe
+        write(fd[1], "Hello, second child process!", 28); // write to the write end of the pipe
+        close(fd[1]); // close the write end of the pipe
+
+        exit(EXIT_SUCCESS);
+    } else { // parent process
+        // fork the second child process
+        pid2 = fork();
+
+        if (pid2 == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid2 == 0) { // second child process
+            close(fd[1]); // close the write end of the pipe
+            read(fd[0], buffer, sizeof(buffer)); // read from the read end of the pipe
+            printf("Second child process received: %s\n", buffer);
+            close(fd[0]); // close the read end of the pipe
+            exit(EXIT_SUCCESS);
+        } else { // parent process
+            close(fd[0]); // close the read end of the pipe
+            write(fd[1], "Hello, first child process!", 27); // write to the write end of the pipe
+            close(fd[1]); // close the write end of the pipe
+            wait(NULL); // wait for the first child process to exit
+            wait(NULL); // wait for the second child process to exit
+            exit(EXIT_SUCCESS);
+        }
+    }
+}
